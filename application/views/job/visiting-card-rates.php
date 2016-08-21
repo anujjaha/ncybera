a32d768c16ecc14efa61b31554214b1869b8f894
Anuj Jaha <er.anujjaha@gmail.com>
2016-08-21 19:34:40 +0530

    Visiting card

---
+++ application/views/job/visiting-card-rates.php
@@ -0,0 +1,681 @@
+<style>
+.gsm300
+{
+	background-color:red !important;
+	border-color:red !important;
+}
+.Vcard_active {
+	font-size:30px;
+}
+</style>
+
+<form action="#" method="post" onsubmit="return saveVCard();">
+<table class="table" border="2">
+	<tr>
+		<td> &nbsp; </td>
+		<td colspan="2" align="center">300 GSM Matt Card </td>
+		<td colspan="2" align="center">350 GSM Matt Card </td>
+		<td colspan="2" align="center">Economy </td>
+		<td colspan="2" align="center">Premium </td>
+		<td colspan="2" align="center">Exclusive </td>
+		<td colspan="2" align="center">Velvet </td>
+	</tr>
+	
+	<tr>
+		<td> Qty </td>
+		<td> Single Side </td>
+		<td> Double Side </td>
+		
+		<td> Single Side </td>
+		<td> Double Side </td>
+		
+		<td> Single Side </td>
+		<td> Double Side </td>
+		
+		<td> Single Side </td>
+		<td> Double Side </td>
+		
+		<td> Single Side </td>
+		<td> Double Side </td>
+		
+		<td> Single Side </td>
+		<td> Double Side </td>
+	</tr>
+	
+	<tr class="rowQty" id="qty_96">
+		<td>
+			<a href="javascript:void(0);" class="btn btn-primary setVQty" onclick="setQty(96);">
+				96
+			</a> 
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsm300" data-price="150" data-card="300_GSM_Matt_Card" data-type="SS">
+				150
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsm300" data-price="300" data-card="300_GSM_Matt_Card" data-type="FB">
+				300
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsm350" data-price="200" data-card="350_GSM_Matt_Card" data-type="SS">
+				200
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsm350" data-price="350" data-card="350_GSM_Matt_Card" data-type="FB">
+				350
+			</span>
+		 </td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="250" data-card="V_Card_Code" data-type="SS">
+				250
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="350" data-card="V_Card_Code" data-type="FB">
+				350
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="300" data-card="V_Card_Code" data-type="SS">
+				300
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="450" data-card="V_Card_Code" data-type="FB">
+				450
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmExclusive" data-price="450" data-card="V_Card_Code" data-type="SS">
+				450
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmExclusive" data-price="700" data-card="V_Card_Code" data-type="FB">
+				700
+			</span>
+		</td>
+		
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="900" data-card="Velvet_Touch_Card" data-type="SS">
+				900
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="900" data-card="Velvet_Touch_Card" data-type="FB">
+				900
+			</span>
+		</td>
+	</tr>
+	
+	<tr  class="rowQty" id="qty_192">
+		<td>
+			<a href="javascript:void(0);" class="btn btn-primary setVQty" onclick="setQty(192);">
+				192
+			</a>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsm300" data-price="250" data-card="300_GSM_Matt_Card" data-type="SS">
+				250
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsm300" data-price="450" data-card="300_GSM_Matt_Card" data-type="FB">
+				450
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsm350" data-price="300" data-card="350_GSM_Matt_Card" data-type="SS">
+				300
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsm350" data-price="500" data-card="350_GSM_Matt_Card" data-type="FB">
+				500
+			</span>
+		 </td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="400" data-card="V_Card_Code" data-type="SS">
+				400
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="550" data-card="V_Card_Code" data-type="FB">
+				550
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="450" data-card="V_Card_Code" data-type="SS">
+				450
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="700" data-card="V_Card_Code" data-type="FB">
+				700
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmExclusive" data-price="700" data-card="V_Card_Code" data-type="SS">
+				700
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmExclusive" data-price="1150" data-card="V_Card_Code" data-type="FB">
+				1150
+			</span>
+		</td>
+		
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="1400" data-card="Velvet_Touch_Card" data-type="SS">
+				1400
+			</span>
+		 </td>
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="1400" data-card="Velvet_Touch_Card" data-type="FB">
+				1400
+			</span>
+		</td>
+	</tr>
+	
+	<tr  class="rowQty" id="qty_288">
+		<td> 
+			<a href="javascript:void(0);" class="btn btn-primary setVQty" onclick="setQty(288);">
+				288
+			</a> 
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsm300" data-price="350" data-card="300_GSM_Matt_Card" data-type="SS">
+				350
+			</span>	
+		</td>
+		<td>
+			<span class="price btn btn-primary gsm300" data-price="600" data-card="300_GSM_Matt_Card" data-type="FB">
+				600
+			</span>
+		 </td>
+		
+		<td> 
+			<span class="price btn btn-primary gsm350" data-price="400" data-card="350_GSM_Matt_Card" data-type="SS">
+				400
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsm350" data-price="700" data-card="350_GSM_Matt_Card" data-type="FB">
+				700
+			</span>
+		 </td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="550" data-card="V_Card_Code" data-type="SS">
+				550
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="750" data-card="V_Card_Code" data-type="FB">
+				750
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="600" data-card="V_Card_Code" data-type="SS">
+				600
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="950" data-card="V_Card_Code" data-type="FB">
+				950
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmExclusive" data-price="1000" data-card="V_Card_Code" data-type="SS">
+				1000
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmExclusive" data-price="1500" data-card="V_Card_Code" data-type="FB">
+				1500
+			</span>
+		</td>
+		
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="1900" data-card="Velvet_Touch_Card" data-type="SS">
+				1900
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="1900" data-card="Velvet_Touch_Card" data-type="FB">
+				1900
+			</span>
+		</td>
+	</tr>
+	
+	<tr  class="rowQty" id="qty_500">
+		<td> 
+			<a href="javascript:void(0);" class="btn btn-primary setVQty" onclick="setQty(500);">
+				500
+			</a> 
+		</td>
+		
+		<td>
+			<span class="price btn btn-primary gsm300" data-price="500" data-card="300_GSM_Matt_Card" data-type="SS">
+				500
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsm300" data-price="750" data-card="300_GSM_Matt_Card" data-type="FB">
+				750
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsm350" data-price="600" data-card="350_GSM_Matt_Card" data-type="SS">
+				600
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsm350" data-price="900" data-card="350_GSM_Matt_Card" data-type="FB">
+				900
+			</span>
+		 </td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="800" data-card="V_Card_Code" data-type="SS">
+				800
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="1250" data-card="V_Card_Code" data-type="FB">
+				1250
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="900" data-card="V_Card_Code" data-type="SS">
+				900
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="1400" data-card="V_Card_Code" data-type="FB">
+				1400
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmExclusive" data-price="1500" data-card="V_Card_Code" data-type="SS">
+				1500
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmExclusive" data-price="2250" data-card="V_Card_Code" data-type="FB">
+				2250
+			</span>
+		</td>
+		
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="2800" data-card="Velvet_Touch_Card" data-type="SS">
+				2800
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="2800" data-card="Velvet_Touch_Card" data-type="FB">
+				2800
+			</span>
+		</td>
+	</tr>
+	
+	<tr  class="rowQty"  id="qty_1000">
+		<td> 
+			<a href="javascript:void(0);" class="btn btn-primary setVQty" onclick="setQty(1000);">
+				1000
+			</a> 
+		</td>
+		
+		<td>
+			<span class="price btn btn-primary gsm300" data-price="900" data-card="300_GSM_Matt_Card" data-type="SS">
+					900
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsm300" data-price="1400" data-card="300_GSM_Matt_Card" data-type="FB">
+				1400
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsm350" data-price="1100" data-card="350_GSM_Matt_Card" data-type="SS">
+				1100
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsm350" data-price="1600" data-card="350_GSM_Matt_Card" data-type="FB">
+				1600
+			</span>
+		 </td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="1500" data-card="V_Card_Code" data-type="SS">
+				1500
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmEconomy" data-price="2250" data-card="V_Card_Code" data-type="FB">
+				2250
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="1700" data-card="V_Card_Code" data-type="SS">
+				1700
+			</span>
+		</td>
+		<td> 
+			<span class="price btn btn-primary gsmPremium" data-price="2700" data-card="V_Card_Code" data-type="FB">
+				2700
+			</span>
+		</td>
+		
+		<td> 
+			<span class="price btn btn-primary gsmExclusive" data-price="2500" data-card="V_Card_Code" data-type="SS">
+				2500
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmExclusive" data-price="3700" data-card="V_Card_Code" data-type="FB">
+				3700
+			</span>
+		</td>
+		
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="5400" data-card="Velvet_Touch_Card" data-type="SS">
+				540
+			</span>
+		</td>
+		<td>
+			<span class="price btn btn-primary gsmVelvet" data-price="5400" data-card="Velvet_Touch_Card" data-type="FB">
+				5400
+			</span>
+		</td>
+	</tr>
+</table>
+
+<table class="table" border="2">
+	<tr>
+		<td> Qty : <span id="show_v_card_qty"></span>
+			<input type="hidden" id="v_card_qty" name="v_card_qty">
+			<input type="hidden" id="original_v_card_qty" name="original_v_card_qty">
+			</td>
+			
+		<td> Paper Type : <span id="show_v_card_type"></span>
+			<input type="hidden" id="v_card_type" name="v_card_type">
+		</td>
+		<td> Estimate : <span id="show_v_card_price"></span>
+			<input type="hidden" name="v_card_price" id="v_card_price">
+			<input type="hidden" id="original_v_card_price" name="original_v_card_price">
+		</td>
+		<td> 
+			Total Names : 
+			<br>
+			<input type="text" id="v_card_names" name="v_card_names" class="form-control" value="1" onBlur="updateQty()"> 
+		</td>
+		<td> 
+			Paper Code : 
+			<br>
+			<input type="text" id="paper_code" name="paper_code" class="form-control" onblur="setPaperCodeValue();"> 
+		</td>
+		<td align="center">
+			<input type="hidden" name="paper_side" id="paper_side">
+			<input type="hidden" name="original_title" id="original_title">
+			<button class="btn btn-primary" onclick="saveVCard();">
+				Confirm
+			</button>
+		</td>
+	</tr>
+</table>
+</form>
+
+<script>
+jQuery(document).ready(function()
+{
+	jQuery(".price").css('opacity','0.10');
+});
+
+jQuery(".gsmExclusive").on('click', function()
+{
+		jQuery("#paper_code").focus();
+});
+
+jQuery(".gsmEconomy").on('click', function()
+{
+		jQuery("#paper_code").focus();
+});
+
+jQuery(".gsmPremium").on('click', function()
+{
+		jQuery("#paper_code").focus();
+});
+
+
+jQuery(".price").on('click', function()
+{
+	jQuery(".price.Vcard_active").removeClass('Vcard_active');
+	
+	setCardPrice(jQuery(this).attr('data-price'));
+	jQuery("#original_v_card_price").val(jQuery(this).attr('data-price'));
+	
+	
+	setCardType(jQuery(this).attr('data-card')+"_"+jQuery(this).attr('data-type'));
+	
+	jQuery("#original_title").val(jQuery(this).attr('data-card')+"_"+jQuery(this).attr('data-type'));
+	
+	jQuery("#paper_side").val(jQuery(this).attr('data-type'));
+	jQuery(this).addClass("Vcard_active");
+	
+	resetInputBox();
+});
+
+
+function resetInputBox()
+{
+	jQuery("#v_card_names").val("1");
+	jQuery("#paper_code").val("");
+}
+
+function setCardPrice(value)
+{
+	jQuery("#v_card_price").val(value);
+	jQuery("#show_v_card_price").html("<h2>"+value+"</h2>");
+}
+
+function setCardType(value)
+{
+	jQuery("#v_card_type").val("V_Card_"+value);
+	jQuery("#show_v_card_type").html("<h2>"+value+"</h2>");
+}
+
+function setQty(value)
+{
+	jQuery(".rowQty").css('opacity', "0.10");
+	
+	jQuery("#qty_"+value+" .price").css('opacity', "1");
+	
+	console.log("Element", jQuery("#qty_"+value+" .price"));
+	jQuery("#qty_"+value).css('opacity', "1");
+	jQuery("#v_card_qty").val(value);
+	jQuery("#original_v_card_qty").val(value);
+	
+	jQuery("#show_v_card_qty").html("<h2>"+value+"</h2>");
+}
+
+function getPaperPrintSide()
+{
+	return jQuery("#paper_side").val();
+}
+function setFinalQty(value)
+{
+	jQuery("#v_card_qty").val(value);
+	jQuery("#show_v_card_qty").html("<h2>"+value+"</h2>");
+}
+
+function getCardQty()
+{
+	return jQuery("#v_card_qty").val();
+}
+
+function getCardType()
+{
+	var cardType 	= jQuery("#v_card_type").val();
+	var paperCode 	= getPaperCode();
+	
+	
+	if(cardType == "V_Card_Code" )
+	{
+		return "V_Card_Code_"+paperCode;
+	}
+	
+	if(cardType == "V_Card_Code" )
+	{
+		return "V_Card_Code_"+paperCode;
+	}
+	
+	if(cardType == "V_Card_Code" )
+	{
+		return "V_Card_Code_"+paperCode;
+	}
+	
+	return jQuery("#v_card_type").val();
+}
+
+function getCardPrice()
+{
+	return jQuery("#v_card_price").val();
+}
+
+function getCardRate()
+{
+	var cardRate = getCardPrice() /getCardQty();
+	
+	return cardRate;
+}
+
+function getPaperCode()
+{
+	return jQuery("#paper_code").val();
+}
+
+function saveVCard()
+{
+	var multiple = 1;
+	
+	if(jQuery("#v_card_names").val().length > 0 )
+	{
+		multiple = jQuery("#v_card_names").val();
+	}
+	
+	var id = jQuery("#fancybox_id").val();
+	jQuery("#category_" +id).val('Visiting Card');
+	
+	if(getPaperCode().length > 0 )
+	{
+		jQuery("#details_" +id).val(getCardType());
+	}
+	else
+	{
+		jQuery("#details_" +id).val(getCardType());
+	}
+	
+	jQuery("#qty_" +id).val(getCardQty());
+	jQuery("#rate_" +id).val(getCardRate());
+	jQuery("#sub_" +id).val(getCardPrice());
+		
+	jQuery.fancybox.close();
+	
+	return false;
+}
+
+function setPaperCodeValue()
+{
+	if(jQuery(".Vcard_active").attr("data-card") == "300_GSM_Matt_Card" )
+	{
+		jQuery("#paper_code").val("");
+		return true;
+	}
+	
+	if(jQuery(".Vcard_active").attr("data-card") == "350_GSM_Matt_Card" )
+	{
+		jQuery("#paper_code").val("");
+		return true;
+	}
+	
+	var paperCode = jQuery("#paper_code").val();
+	var printSide = getPaperPrintSide();
+	
+	if(jQuery("#v_card_names").length > 0 )
+	{
+		var originalQty = jQuery("#original_v_card_qty").val();
+		var multiple 	= jQuery("#v_card_names").val();
+		var finalTitle = "V_Card_Code_"+paperCode+"_"+printSide+"_"+originalQty+"*"+multiple;
+		
+		jQuery("#v_card_type").val(finalTitle);
+		jQuery("#show_v_card_type").html("<h2>"+finalTitle+"</h2>");
+		
+		return true;
+	}
+	
+	jQuery("#v_card_type").val("V_Card_Code_"+paperCode+"_"+printSide);
+	jQuery("#show_v_card_type").html("<h2>"+"V_Card_Code_"+paperCode+"_"+printSide+"</h2>");
+}
+
+function updateQty()
+{
+	var multiple = 1;
+	
+	if(jQuery("#v_card_names").val().length > 0 )
+	{
+		multiple = jQuery("#v_card_names").val();
+	}
+	
+	setFinalQty(jQuery("#original_v_card_qty").val() * multiple)
+	setCardPrice(jQuery("#original_v_card_price").val() * multiple);
+	
+	var cardTitle = jQuery("#original_title").val();
+	
+	var paperCode = jQuery("#paper_code").val();
+	var printSide = getPaperPrintSide();
+	
+	if(jQuery("#paper_code").val().length > 0) 
+	{
+		
+		
+		
+		console.log("OrignalTilte", cardTitle);
+		
+		var finalTitle = "V_Card_Code_"+paperCode+"_"+printSide+"_"+jQuery("#original_v_card_qty").val()+"*"+multiple;
+		
+		setCardType(finalTitle);
+		
+		return true;
+	}
+	
+	var finalTitle = "V_Card_Code_"+printSide+"_"+jQuery("#original_v_card_qty").val()+"*"+multiple;
+	
+	
+	setCardType(cardTitle);
+		
+	return true;
+}
+</script>
